926bd15f6b5334b654b613a0c8609d9c2c359b37
Thomas Wielandt <wielants@github.com>
2016-09-02 21:48:44 +0200

    added 5.7 excercises

--- src/chapter5/exercises_5_7_type_inference.hs
+++ src/chapter5/exercises_5_7_type_inference.hs
@@ -1,3 +1,33 @@
 module Excercises_5_7_Type_Inference where
 
+import Prelude hiding ((++),(*),take,(>),(<))
 
+-- 1)
+(++) :: [a] -> [a] -> [a]
+(++) = undefined
+-- myConcat :: [Char] -> [Char]
+myConcat x = x ++ "yo"
+
+-- 2)
+(*) :: Num a => a -> a -> a
+(*) = undefined
+-- myMult :: Fractional a => a -> a
+myMult x = (x / 3) * 5
+
+-- 3)
+take :: Int -> [a] -> [a]
+take = undefined
+-- myTake :: [Int] -> [Char]
+myTake x = take x "hey you"
+
+-- 4)
+(>) :: Ord a => a -> a -> Bool
+(>) = undefined
+-- myCom :: Int -> Bool
+myCom x = x > (length [1..10])
+
+-- 5)
+(<) :: Ord a => a -> a -> Bool
+(<) = undefined
+-- myAlph :: Char -> Bool
+myAlph x = x < 'z'
