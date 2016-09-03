d4705f2d334c27114ff7fbe646cf2c8a4966ef34
Thomas Wielandt <wielants@github.com>
2016-09-03 23:46:42 +0200

    fix it 1) corrected version

--- src/chapter5/exercises_5_9_fix_it.hs
+++ src/chapter5/exercises_5_9_fix_it.hs
@@ -1,11 +1,11 @@
-module sing where
+module Sing where
 
-fstString :: [Char] ++ [Char]
+fstString :: [Char] -> [Char]
 fstString x = x ++ " in the rain"
 
-sndString :: [Char] -> Char
+sndString :: [Char] -> [Char]
 sndString x = x ++ " over the rainbow"
 
-sing = if (x > y) then fstString x or sndString y
-where x = "Singin"
-      x = "Somewhere"
+sing = if (x > y) then fstString x else sndString y
+  where x = "Singin"
+        y = "Somewhere"
